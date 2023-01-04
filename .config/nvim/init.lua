c4be41cb55748acf13f490cf6b171607a1d073d8
Eric Baukhages <eric.baukhages@gmail.com>
2023-01-04 09:49:04 -0500

    Fix references to luasnip and cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -274,28 +274,28 @@ require('fidget').setup()
 require('cmp').setup({
 	snippet = {
 		expand = function(args)
-			luasnip.lsp_expand(args.body)
+			require('luasnip').lsp_expand(args.body)
 		end,
 	},
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
+	mapping = require('cmp').mapping.preset.insert({
+		['<CR>'] = require('cmp').mapping.confirm({
+			behavior = require('cmp').ConfirmBehavior.Replace,
 			select = true,
 		}),
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
+		['<Tab>'] = require('cmp').mapping(function(fallback)
+			if require('cmp').visible() then
+				require('cmp').select_next_item()
+			elseif require('luasnip').expand_or_jumpable() then
+				require('luasnip').expand_or_jump()
 			else
 				fallback()
 			end
 		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
+		['<S-Tab>'] = require('cmp').mapping(function(fallback)
+			if require('cmp').visible() then
+				require('cmp').select_prev_item()
+			elseif require('luasnip').jumpable(-1) then
+				require('luasnip').jump(-1)
 			else
 				fallback()
 			end
