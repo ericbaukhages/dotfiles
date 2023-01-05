0a939f9555671490e875ea4a725ffb96ac1e7ac9
Eric Baukhages <eric.baukhages@gmail.com>
2023-01-05 09:33:23 -0500

    Add more indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,120 +38,120 @@
 --}}}
 
 -- plugins {{{1
--- install packer {{{2
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-local install_plugins = false
+	-- install packer {{{2
+		local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+		local install_plugins = false
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	print('Installing packer...')
-	local packer_url = 'https://github.com/wbthomason/packer.nvim'
-	vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
-	print('Done.')
+		if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+			print('Installing packer...')
+			local packer_url = 'https://github.com/wbthomason/packer.nvim'
+			vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+			print('Done.')
 
-	vim.cmd('packadd packer.nvim')
-	install_plugins = true
-end
--- Run this to check where plugins are installed
--- `:echo stdpath('data') . '/site/pack/packer'
-
---}}}
-
--- packer plugins {{{2
-require('packer').startup(function(use)
-
-	-- package manager
-	use 'wbthomason/packer.nvim'
-
-	-- chrome refresh plugin
-	use 'ericbaukhages/vim-refresh'
-
-	-- tpope plugins! {{{3
-	use 'tpope/vim-abolish'
-	use 'tpope/vim-commentary'
-	use 'tpope/vim-eunuch'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-repeat'
-	use 'tpope/vim-sleuth'
-	use 'tpope/vim-surround'
-	use 'tpope/vim-vinegar'
-	---}}}
-
-	--- lsp {{{3
-	use {
-		'neovim/nvim-lspconfig',
-		requires = {
-			-- Automatically  install LSPs to stdpath for neovim
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-
-			-- Useful status updates for LSP
-			'j-hui/fidget.nvim',
-
-			-- Additional lua configuration
-			'folke/neodev.nvim',
-		}
-	}
-	---}}}
-
-	-- autocompletion {{{3
-	use {
-		'hrsh7th/nvim-cmp',
-		requires = {
-			'hrsh7th/cmp-nvim-lsp',
-			'L3MON4D3/LuaSnip',
-			'saadparwaiz1/cmp_luasnip',
-		}
-	}
-	--}}}
-
-	-- treesitter {{{3
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+			vim.cmd('packadd packer.nvim')
+			install_plugins = true
 		end
-	}
+		-- Run this to check where plugins are installed
+		-- `:echo stdpath('data') . '/site/pack/packer'
+
 	--}}}
 
-	use 'nvim-lualine/lualine.nvim'
-	use 'lewis6991/gitsigns.nvim'
+	-- packer plugins {{{2
+		require('packer').startup(function(use)
 
-	-- telescope {{{3
-	use {
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		requires = {
-			'nvim-lua/plenary.nvim',
-		}
-	}
+			-- package manager
+			use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope-fzf-native.nvim',
-		run = 'make',
-		cond = vim.fn.executable('make') == 1
-	}
+			-- chrome refresh plugin
+			use 'ericbaukhages/vim-refresh'
+
+			-- tpope plugins! {{{3
+				use 'tpope/vim-abolish'
+				use 'tpope/vim-commentary'
+				use 'tpope/vim-eunuch'
+				use 'tpope/vim-fugitive'
+				use 'tpope/vim-repeat'
+				use 'tpope/vim-sleuth'
+				use 'tpope/vim-surround'
+				use 'tpope/vim-vinegar'
+			---}}}
+
+			--- lsp {{{3
+				use {
+					'neovim/nvim-lspconfig',
+					requires = {
+						-- Automatically  install LSPs to stdpath for neovim
+						'williamboman/mason.nvim',
+						'williamboman/mason-lspconfig.nvim',
+
+						-- Useful status updates for LSP
+						'j-hui/fidget.nvim',
+
+						-- Additional lua configuration
+						'folke/neodev.nvim',
+					}
+				}
+			---}}}
+
+			-- autocompletion {{{3
+				use {
+					'hrsh7th/nvim-cmp',
+					requires = {
+						'hrsh7th/cmp-nvim-lsp',
+						'L3MON4D3/LuaSnip',
+						'saadparwaiz1/cmp_luasnip',
+					}
+				}
+			--}}}
+
+			-- treesitter {{{3
+				use {
+					'nvim-treesitter/nvim-treesitter',
+					run = function()
+						pcall(require('nvim-treesitter.install').update({ with_sync = true }))
+					end
+				}
+			--}}}
+
+			use 'nvim-lualine/lualine.nvim'
+			use 'lewis6991/gitsigns.nvim'
+
+			-- telescope {{{3
+				use {
+					'nvim-telescope/telescope.nvim',
+					branch = '0.1.x',
+					requires = {
+						'nvim-lua/plenary.nvim',
+					}
+				}
+
+				use {
+					'nvim-telescope/telescope-fzf-native.nvim',
+					run = 'make',
+					cond = vim.fn.executable('make') == 1
+				}
+			--}}}
+
+			local has_plugins, plugins = pcall(require, 'custom.plugins')
+			if has_plugins then
+				plugins(use)
+			end
+
+			if install_plugins then
+				require('packer').sync()
+			end
+		end)
 	--}}}
 
-	local has_plugins, plugins = pcall(require, 'custom.plugins')
-	if has_plugins then
-		plugins(use)
-	end
-
+	-- don't continue if installing plugins for the first time
 	if install_plugins then
-		require('packer').sync()
+		print('===================================')
+		print('    Plugins are being installed    ')
+		print('    Wait until Packer completes,   ')
+		print('       then restart nvim           ')
+		print('===================================')
+		return
 	end
-end)
---}}}
-
--- don't continue if installing plugins for the first time
-if install_plugins then
-	print('===================================')
-	print('    Plugins are being installed    ')
-	print('    Wait until Packer completes,   ')
-	print('       then restart nvim           ')
-	print('===================================')
-	return
-end
 
 --}}}
 
