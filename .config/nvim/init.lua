927ca95d4660a3df54e0442d280379e6e9766f8d
Eric Baukhages <eric.baukhages@gmail.com>
2023-01-05 09:35:54 -0500

    Add final bit of indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,155 +157,155 @@
 
 -- Plugin settings {{{1
 
--- telescope {{{2
--- see `:help telescope` and `:help telescope.setup()`
-require('telescope').setup({
-	defaults = {
-		mappings = {
-			i = {
-				['<C-u>'] = false,
-				['<C-d>'] = false,
-			}
-		}
-	},
-})
+	-- telescope {{{2
+		-- see `:help telescope` and `:help telescope.setup()`
+		require('telescope').setup({
+			defaults = {
+				mappings = {
+					i = {
+						['<C-u>'] = false,
+						['<C-d>'] = false,
+					}
+				}
+			},
+		})
 
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
+		-- Enable telescope fzf native, if installed
+		pcall(require('telescope').load_extension, 'fzf')
 
--- keymaps {{{3
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope').get_dropdown({
-		winblend = 10,
-		previewer = false,
-	}))
-end, { desc = '[/] Fuzzily search in current buffer' })
+		-- keymaps {{{3
+			-- See `:help telescope.builtin`
+			vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+			vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+			vim.keymap.set('n', '<leader>/', function()
+				require('telescope.builtin').current_buffer_fuzzy_find(require('telescope').get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
---}}}
+			vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+			vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+			vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+			vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+			vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+		--}}}
 
---}}}
-
--- treesitter {{{2
--- see `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup({
-	ensure_installed = { 'typescript', 'css', 'help', 'vim', 'html' },
-
-	highlight = { enable = true },
-	indent = { enable = true },
-})
---}}}
-
--- lsp {{{2
-local on_attach = function(_, bufnr)
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = 'LSP: ' .. desc
-		end
-		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-	end
-
-	-- keymaps {{{3
-	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [D]efinition')
-	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-
-	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-	nmap('<leader>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, '[W]orkspace [L]ist Folders')
 	--}}}
 
-	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-		vim.lsp.buf.format()
-	end, { desc = 'Format current buffer with LSP' })
-end
+	-- treesitter {{{2
+		-- see `:help nvim-treesitter`
+		require('nvim-treesitter.configs').setup({
+			ensure_installed = { 'typescript', 'css', 'help', 'vim', 'html' },
 
-require('neodev').setup()
-require('mason').setup()
-
-local lsp_servers = {
-	cssls = {},
-	emmet_ls = {},
-	eslint = {},
-	html = {},
-	jsonls = {},
-	tailwindcss = {},
-	tsserver = {},
-}
-
-require('mason-lspconfig').setup({
-	ensure_installed = vim.tbl_keys(lsp_servers)
-})
-
-require('mason-lspconfig').setup_handlers({
-	function(server_name)
-		require('lspconfig')[server_name].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = lsp_servers[server_name],
+			highlight = { enable = true },
+			indent = { enable = true },
 		})
-	end
-})
+	--}}}
 
-require('fidget').setup()
-
-require('cmp').setup({
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	mapping = require('cmp').mapping.preset.insert({
-		['<CR>'] = require('cmp').mapping.confirm({
-			behavior = require('cmp').ConfirmBehavior.Replace,
-			select = true,
-		}),
-		['<Tab>'] = require('cmp').mapping(function(fallback)
-			if require('cmp').visible() then
-				require('cmp').select_next_item()
-			elseif require('luasnip').expand_or_jumpable() then
-				require('luasnip').expand_or_jump()
-			else
-				fallback()
+	-- lsp {{{2
+		local on_attach = function(_, bufnr)
+			local nmap = function(keys, func, desc)
+				if desc then
+					desc = 'LSP: ' .. desc
+				end
+				vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = require('cmp').mapping(function(fallback)
-			if require('cmp').visible() then
-				require('cmp').select_prev_item()
-			elseif require('luasnip').jumpable(-1) then
-				require('luasnip').jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	}),
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	},
-})
 
---}}}
+			-- keymaps {{{3
+				nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+				nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+				nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+				nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [D]efinition')
+				nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+
+				nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+				nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+				nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+				nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+				nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+				nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+				nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+				nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+				nmap('<leader>wl', function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, '[W]orkspace [L]ist Folders')
+			--}}}
+
+			vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+				vim.lsp.buf.format()
+			end, { desc = 'Format current buffer with LSP' })
+		end
+
+		require('neodev').setup()
+		require('mason').setup()
+
+		local lsp_servers = {
+			cssls = {},
+			emmet_ls = {},
+			eslint = {},
+			html = {},
+			jsonls = {},
+			tailwindcss = {},
+			tsserver = {},
+		}
+
+		require('mason-lspconfig').setup({
+			ensure_installed = vim.tbl_keys(lsp_servers)
+		})
+
+		require('mason-lspconfig').setup_handlers({
+			function(server_name)
+				require('lspconfig')[server_name].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+					settings = lsp_servers[server_name],
+				})
+			end
+		})
+
+		require('fidget').setup()
+
+		require('cmp').setup({
+			snippet = {
+				expand = function(args)
+					require('luasnip').lsp_expand(args.body)
+				end,
+			},
+			mapping = require('cmp').mapping.preset.insert({
+				['<CR>'] = require('cmp').mapping.confirm({
+					behavior = require('cmp').ConfirmBehavior.Replace,
+					select = true,
+				}),
+				['<Tab>'] = require('cmp').mapping(function(fallback)
+					if require('cmp').visible() then
+						require('cmp').select_next_item()
+					elseif require('luasnip').expand_or_jumpable() then
+						require('luasnip').expand_or_jump()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+				['<S-Tab>'] = require('cmp').mapping(function(fallback)
+					if require('cmp').visible() then
+						require('cmp').select_prev_item()
+					elseif require('luasnip').jumpable(-1) then
+						require('luasnip').jump(-1)
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+			}),
+			sources = {
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			},
+		})
+
+	--}}}
 --}}}
 
 -- vim: fdm=marker ts=2 sts=2 sw=2
