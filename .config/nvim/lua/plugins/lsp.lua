4cdc33fc3cd2fe1f22b6ac72454c334f5b352ebb
Eric Baukhages <eric.baukhages@gmail.com>
2023-10-03 00:29:14 -0400

    Add auto-formatter

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -72,4 +72,47 @@ return {
 			})
 		end
 	},
+
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			require("formatter").setup({
+				logging = true,
+				log_level = vim.log.levels.WARN,
+				filetype = {
+					html = {
+						require("formatter.filetypes.html").prettier,
+					},
+					css = {
+						require("formatter.filetypes.css").prettier,
+					},
+					json = {
+						require("formatter.filetypes.json").prettier,
+					},
+					javascript = {
+						require("formatter.filetypes.javascript").prettier,
+					},
+					javascriptreact = {
+						require("formatter.filetypes.javascriptreact").prettier,
+					},
+					typescript = {
+						require("formatter.filetypes.typescript").prettier,
+					},
+					typescriptreact = {
+						require("formatter.filetypes.typescriptreact").prettier,
+					},
+					["*"] = {
+						require("formatter.filetypes.any").remove_trailing_whitespace
+					}
+				}
+			})
+
+			vim.api.nvim_create_augroup("FormatAutogroup", { clear = true });
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				group = "FormatAutogroup",
+				pattern = "*",
+				command = "FormatWrite",
+			})
+		end,
+	},
 }
