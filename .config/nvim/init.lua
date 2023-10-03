3d631f69174b097b53aa7b79f0477046b9cc917f
Eric Baukhages <eric.baukhages@gmail.com>
2023-10-02 22:06:06 -0400

    Readd just lazy.nvim as a package manager, and add basic plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,4 +37,22 @@
 	--}}}
 --}}}
 
+-- plugins {{{1
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+	
+	-- load plugin list from ~/.config/nvim/lua/plugins/init.lua
+	require("lazy").setup("plugins");
+--{{{
+
 -- vim: fdm=marker ts=2 sts=2 sw=2
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+	'ericbaukhages/vim-refresh',
+
+	'tpope/vim-abolish',
+	'tpope/vim-commentary',
+	'tpope/vim-eunuch',
+	'tpope/vim-fugitive',
+	'tpope/vim-repeat',
+	'tpope/vim-sleuth',
+	'tpope/vim-surround',
+	'tpope/vim-vinegar',
+}
