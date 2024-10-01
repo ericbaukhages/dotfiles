e1954b36a12f957d86d8a13dda024213a3493bb0
Eric Baukhages <eric.baukhages@gmail.com>
2024-10-01 09:02:00 -0400

    Continue to make the configs more utility focused

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,8 +1,9 @@
 return {
+	"folke/which-key.nvim",
+
 	"tpope/vim-abolish",
 	"tpope/vim-commentary",
 	"tpope/vim-eunuch",
-	"tpope/vim-fugitive",
 	"tpope/vim-repeat",
 	"tpope/vim-sleuth",
 	"tpope/vim-surround",
