0aa0ce38c2a1ad934ac03df00907c05276b137c1
Eric Baukhages <eric.baukhages@gmail.com>
2024-10-14 11:26:43 -0400

    Update LSP, Telescope, and Tresitter configs
    Setting the stage for centralized configurations

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,14 +14,7 @@ return {
 				}
 			})
 
-			pcall(require('telescope').load_extension, 'ui-select')
-
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			pcall(require("telescope").load_extension, "ui-select")
 		end
 	},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,18 @@
+local languages = {
+	"javascript",
+	"lua",
+	"markdown",
+	"typescript",
+	"yaml",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "markdown", "javascript", "typescript" },
+			ensure_installed = languages,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
