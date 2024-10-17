b7f7e7e06ca580ec92b6ea9d79c1f1b876defb06
Eric Baukhages <eric.baukhages@gmail.com>
2024-10-17 09:06:30 -0400

    Bring the non LSP mappings out of the LSPAttach autocmd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -30,30 +30,36 @@ return {
 				lspconfig[server].setup({})
 			end
 
+			local builtin = require("telescope.builtin")
+			local map = function(keys, func, desc, mode)
+				mode = mode or "n"
+				vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+			end
+
+			map("<leader>sf", builtin.find_files, "[S]earch [F]iles")
+			map("<leader>sg", builtin.live_grep, "[S]earch by [G]rep")
+			map("<leader><leader>", builtin.buffers, "[ ] Find existing buffers")
+			map("<leader>sh", builtin.help_tags, "[S]earch [H]elp")
+			map("<leader>sd", builtin.diagnostics, "[S]earch [D]iagnostics")
+
+			map("gD", builtin.lsp_type_definitions, "[G]oto Type [D]efinition")
+
+			map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+			map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+			map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					local builtin = require("telescope.builtin")
 					local b = vim.lsp.buf
-					local map = function(keys, func, desc, mode)
+					map = function(keys, func, desc, mode)
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
 					end
 
 					map("K", b.hover, "[K] Hover")
 					map("<leader>ca", b.code_action, "[C]ode [A]ction", { "n", "x" })
-
-					map("<leader>sf", builtin.find_files, "[S]earch [F]iles")
-					map("<leader>sg", builtin.live_grep, "[S]earch by [G]rep")
-					map("<leader><leader>", builtin.buffers, "[ ] Find existing buffers")
-					map("<leader>sh", builtin.help_tags, "[S]earch [H]elp")
-					map("<leader>sd", builtin.diagnostics, "[S]earch [D]iagnostics")
-
-					map("gD", builtin.lsp_type_definitions, "[G]oto Type [D]efinition")
-
-					map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
-					map("gr", builtin.lsp_references, "[G]oto [R]eferences")
-					map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>rn", b.rename, "[R]e[n]ame")
 				end
 			})
 		end
