88dae8ee8c0514db30b3b70cec57f301b293ef79
Eric Baukhages <eric.baukhages@gmail.com>
2025-09-26 10:26:07 -0400

    Make LSP settings more explicit

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,3 @@
-local servers = {
-	"lua_ls",
-	"ts_ls",
-	"eslint",
-}
-
 return {
 	{
 		"williamboman/mason.nvim",
@@ -15,7 +9,11 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = servers
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"eslint",
+				}
 			})
 		end
 	},
@@ -25,16 +23,61 @@ return {
 			"nvim-telescope/telescope.nvim",
 		},
 		config = function()
-			local lspconfig = require("lspconfig")
+			vim.lsp.config("lua_ls", {
+				on_init = function(client)
+					-- If the workspace has a luarc config, exit early
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+								path ~= vim.fn.stdpath("config")
+								and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
+					end
 
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({})
-			end
+					-- Set Lua rules, assuming this is the neovim config
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+							path = {
+								"lua/?.lua",
+								"lua/?/init.lua",
+							},
+						},
+						workspace = {
+							checkThirdParty = false,
+							library = {
+								vim.env.VIMRUNTIME
+							}
+						}
+					})
+				end,
+				settings = {
+					Lua = {}
+				}
+			})
+
+			vim.lsp.config("eslint", {
+				on_attach = function(client, bufnr)
+					if not vim.lsp.config.eslint.on_attach then return end
+
+					vim.lsp.config.eslint.on_attach(client, bufnr)
+
+					-- FIXME: figure out why this isn't working as expected
+					-- vim.api.nvim_create_autocmd("BufWritePre", {
+					-- 	buffer = bufnr,
+					-- 	command = "LspEslintFixAll",
+					-- })
+				end,
+			})
+
+			vim.lsp.enable("ts_ls")
 
 			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-				callback = function(event)
-					require("../keybindings").lsp(event.buf)
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function()
+					require("../keybindings").lsp()
 				end
 			})
 		end
