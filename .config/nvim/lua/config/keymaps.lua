24ecce818e0370c9da37eba25f2b775e77746eef
Eric Baukhages <eric.baukhages@gmail.com>
2025-10-15 12:51:58 -0400

    Add conform and run it on the config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,14 @@
 -- Only use the `<C-a>` command, as `<C-e>` already works as `<End>`
-vim.keymap.set('c', '<C-a>', '<Home>', {})
+vim.keymap.set("c", "<C-a>", "<Home>", {})
 
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
 
-vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end,
-    {desc="Open Diagnostics in Float"}
-)
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "Open Diagnostics in Float" })
+
+vim.keymap.set("n", "<leader>cf", function()
+	require("conform").format({
+		lsp_format = "fallback",
+	})
+end, { desc = "Format current file" })
